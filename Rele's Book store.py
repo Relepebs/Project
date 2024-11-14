399553b598b2b55106e205cafa5cc67491c156a3
babyashwaggggg1 <kduba@putnamscience.org>
2024-11-14 13:25:30 -0500

    Update Rele's Book store.py

--- Rele's Book store.py
+++ Rele's Book store.py
@@ -1,5 +1,29 @@
+STR = True
 print("Book Store")
-print("Name:\nMoving in circles but not in lanes")
-print("price:\n$60")
+list== ("1. The lie\n2.Twilight\n3.Levidicus\n4.Passion twist\n5.Pink")
+books = input("Choose book""(1. The lie\n2.Twilight\n3.Levidicus\n4.Passion twist\n5.Pink)")
+book1 = "1.The lie"
+book2 = "2.Twilight"
+book3 = "3.Levidicus"
+book4 = "4.Passion twist"
+book5 = "5.Pink"
+if books == book1:
+    print("Author:\nPrice:$\nPlot:")
+if books == book2:
+    print("Author:\nPrice:$\nPlot:")
 
-storage= input("file")
+
+
+
+
+
+
+
+
+
+
+
+
+"""while STR:
+    print("Escape Book Store")
+    print("1. The lie\n2.Twilight\n3.Levidicus\n4.Passion twist\n5.Pink")"""
